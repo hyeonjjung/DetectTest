74396dbcacf72d8b99f086ca212f5b7404226855
userp <jhikm1003@gmail.com>
2018-04-23 04:45:01 +0900

    add transfer data by beacon

--- app/src/main/java/com/example/userp/detecttest/GPSController.java
+++
@@ -1,8 +0,0 @@
-package com.example.userp.detecttest;
-
-/**
- * Created by userp on 2018-04-23.
- */
-
-public class GPSController {
-}
--- app/src/main/java/com/example/userp/detecttest/MainActivity.java
+++ app/src/main/java/com/example/userp/detecttest/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -34,6 +35,7 @@ import org.altbeacon.beacon.Region;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer {
@@ -43,35 +45,40 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 101;
 
     private static final String BEACON_PARSER = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25";
+    private static final String MY_UUID = "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6";
 
-    TextView txtview, Locationtxtview, beaconTxtview;
+    private TextView txtview, Locationtxtview, beaconTxtview, resultTxtview;
+    private Button startBeaconMonitoringBtn;
 
     BeaconController beaconController;
 
-    private Button startBtn = null;
-    private Button startBeaconBtn = null;
-    private Button startBeaconMonitoringBtn = null;
-
     private BeaconManager beaconManager = null;
 
+    private LocationManager locationManager = null;
+    private LocationListener locationListener = null;
+
+    private double currentSpeed = 0;
+    private static int GPS_UPDATE_MIN_DISTANCE = 0;
+    private static int GPS_UPDATE_MIN_TIME = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         txtview = (TextView) findViewById(R.id.speedTextView);
         beaconTxtview = (TextView) findViewById(R.id.findedBeaconTextView);
+        resultTxtview = (TextView) findViewById(R.id.resultTextView);
 
-        startBtn = (Button) findViewById(R.id.startBtn);
-        startBeaconBtn = (Button) findViewById(R.id.startBeaconBtn);
-        startBeaconMonitoringBtn = (Button) findViewById(R.id.startBeaconMonitoringBtn);
-
-
+        startBeaconMonitoringBtn = (Button)findViewById(R.id.startBeaconMonitoringBtn);
         beaconController = new BeaconController();
 
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BEACON_PARSER));
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new SpeedActionListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, GPS_UPDATE_MIN_TIME, GPS_UPDATE_MIN_DISTANCE, locationListener);
 
         //For permission check
         try {
@@ -92,20 +99,6 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        startBeaconBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                beaconController.startBeaconTransmitter(getApplicationContext());
-            }
-        });
 
         startBeaconMonitoringBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,16 +121,28 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
                 if(collection.size() > 0) {
+                    Log.d(TAG, "Find Beacon !");
                     Iterator<Beacon> iterator = collection.iterator();
                     while(iterator.hasNext()) {
                         Beacon beacon = iterator.next();
-                        String address = beacon.getBluetoothAddress();
-                        double rssi = beacon.getRssi();
-                        int txPower = beacon.getTxPower();
-                        UUID id1 = beacon.getId1().toUuid();
+                        UUID uuid = beacon.getId1().toUuid();
                         int major = beacon.getId2().toInt();
                         int minor = beacon.getId3().toInt();
+                        Log.d(TAG, beacon.getDataFields().get(0).toString());
+                        if(uuid.toString() == MY_UUID) {
+
+                        }
                     }
+                } else {
+                    Log.d(TAG, "I can't find beacon...");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.d(TAG, "onRunnable()");
+                            resultTxtview.setText("I'm Driver!");
+
+                        }
+                    });
                 }
             }
         });
@@ -148,12 +153,40 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     public void startBeaconScan() {
         if(!beaconManager.isBound(MainActivity.this)) {
             beaconManager.bind(MainActivity.this);
-            Log.d(TAG, "start Beacon monitoring...");
         }
     }
     public void stopBeaconScan() {
         if(beaconManager.isBound(MainActivity.this)) {
             beaconManager.unbind(MainActivity.this);
+        }
+    }
+
+    private class SpeedActionListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            if(location!= null) {
+                currentSpeed = location.getSpeed() * 3.6;
+                txtview.setText("Current speed : "+currentSpeed+"km/h");
+                if(currentSpeed > 20) {
+                    startBeaconScan();
+                }
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
         }
     }
 }
